f36b1eec78bd493ffaed855225eb28b05b0c6784
estebandsilva <137509594+estebandsilva@users.noreply.github.com>
2023-09-07 11:09:19 +0200

    Update pruebaqt.sh

--- pruebaqt.sh
+++ pruebaqt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo su
 
-directorio_buscado="PyQT"
+directorio_buscado="GUI"
 ruta_especificada="/home/mequonic/Desktop"
 
 ruta_directorio="$ruta_especificada/$directorio_buscado"
@@ -10,9 +10,9 @@ if [ -d "$ruta_directorio" ]; then
     echo "Se encontró el directorio '$directorio_buscado' en la ruta '$ruta_esp>
     # Haz algo si el directorio existe
     # ... añade aquí la rutina que deseas ejecutar
-    cd /home/mequonic/Desktop/PyQT
+    cd /home/mequonic/Desktop/GUI
     git pull
-    git checkout modify6
+    # git checkout modify6
     python main_graf.py
     read
 
@@ -20,6 +20,6 @@ else
     echo "No se encontró el directorio '$directorio_buscado' en la ruta '$ruta_>
     # Haz algo si el directorio no existe
     # ... añade aquí la otra rutina que deseas ejecutar
-    git clone https://github.com/estebandsilva/PyQT
+    git clone https://github.com/estebandsilva/GUI
 
 fi
