15adf12a2e1f27e3dd6779fd8eba39c9368c8761
estebandsilva <137509594+estebandsilva@users.noreply.github.com>
2023-09-07 11:03:51 +0200

    Create pruebaqt.sh
    with qt.sh

---
+++ pruebaqt.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+directorio_buscado="PyQT"
+ruta_especificada="/home/mequonic/Desktop"
+
+ruta_directorio="$ruta_especificada/$directorio_buscado"
+
+if [ -d "$ruta_directorio" ]; then
+    echo "Se encontró el directorio '$directorio_buscado' en la ruta '$ruta_esp>
+    # Haz algo si el directorio existe
+    # ... añade aquí la rutina que deseas ejecutar
+    cd /home/mequonic/Desktop/PyQT
+    git pull
+    git checkout modify6
+    python main_graf.py
+    read
+
+else
+    echo "No se encontró el directorio '$directorio_buscado' en la ruta '$ruta_>
+    # Haz algo si el directorio no existe
+    # ... añade aquí la otra rutina que deseas ejecutar
+    git clone https://github.com/estebandsilva/PyQT
+
+fi
