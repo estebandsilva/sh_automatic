dfcc8aebfe6dc91e32a5a4aecbf5e93fd4b4d9bf
estebandsilva <137509594+estebandsilva@users.noreply.github.com>
2023-09-07 11:27:07 +0200

    Update pruebaqt.sh

--- pruebaqt.sh
+++ pruebaqt.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
-cd ~/Desktop
-
 directorio_buscado="GUI"
 ruta_especificada="/home/mequonic/Desktop"
 
 ruta_directorio="$ruta_especificada/$directorio_buscado"
 
 if [ -d "$ruta_directorio" ]; then
-    echo "Se encontró el directorio '$directorio_buscado' en la ruta '$ruta_esp>
+    echo "Se encontró el directorio '$directorio_buscado' en la ruta '$ruta_especificada'."
     # Haz algo si el directorio existe
     # ... añade aquí la rutina que deseas ejecutar
     cd /home/mequonic/Desktop/GUI
     git pull
-    # git checkout modify6
+    git checkout main
     python main_graf.py
     read
 
+
 else
-    echo "No se encontró el directorio '$directorio_buscado' en la ruta '$ruta_>
+    echo "No se encontró el directorio '$directorio_buscado' en la ruta '$ruta_especificada'."
     # Haz algo si el directorio no existe
     # ... añade aquí la otra rutina que deseas ejecutar
     git clone https://github.com/estebandsilva/GUI
