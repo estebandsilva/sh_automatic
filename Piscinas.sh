36cb4f91870207b32704718f1cc3e23dde50777f
estebandsilva <137509594+estebandsilva@users.noreply.github.com>
2024-07-24 13:42:17 +0200

    Create Piscinas.sh

---
+++ Piscinas.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+directorio_buscado="Piscinas"
+ruta_especificada="/home/mequonic/Desktop"
+
+ruta_directorio="$ruta_especificada/$directorio_buscado"
+
+if [ -d "$ruta_directorio" ]; then
+    echo "Se encontró el directorio '$directorio_buscado' en la ruta '$ruta_especificada'."
+    # Haz algo si el directorio existe
+    # ... añade aquí la rutina que deseas ejecutar
+    cd /home/mequonic/Desktop/Piscinas
+    git pull
+    git checkout main
+    python main.py
+    read
+
+
+else
+    echo "No se encontró el directorio '$directorio_buscado' en la ruta '$ruta_especificada'."
+    # Haz algo si el directorio no existe
+    # ... añade aquí la otra rutina que deseas ejecutar
+    git clone https://github.com/estebandsilva/Assabloy_Code
+
+fi
